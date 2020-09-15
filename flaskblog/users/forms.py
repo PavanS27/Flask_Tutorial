1d6e690b475222542922a20275af693606bf3d13
PavanS27 <48400643+PavanS27@users.noreply.github.com>
2020-09-15 16:29:14 +0530

    blueprints

--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,10 +1,9 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField,FileAllowed
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField,TextAreaField
-from wtforms.validators import DataRequired, Length, Email, EqualTo,ValidationError
 from flaskblog.models import User
-
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[
@@ -52,10 +51,6 @@ class UpdateAccountForm(FlaskForm):
                 raise ValidationError('Email already exists!!')
 
 
-class PostForm(FlaskForm):
-    title = StringField('Title',validators = [DataRequired()])
-    content = TextAreaField('Content',validators=[DataRequired()])
-    submit = SubmitField('Post')
 
 class RequestResetForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
@@ -73,6 +68,4 @@ class ResetPasswordForm(FlaskForm):
                                      DataRequired(), EqualTo("password")]
                                     )
     submit = SubmitField('Reset Password')
-
-
 
